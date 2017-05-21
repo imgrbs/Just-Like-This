ed68aa01a21fb2395a98c7b5dd5f3fec063a5697
ImagineRabbits <mixxdarkz2@gmail.com>
2017-05-21 21:55:37 +0700

    add pic , add path in json

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 
 var globalData;
 
-var getOnStart = ()=>{
+var getOnStart = () =>{
     $.getJSON('./assets/json/showcase/data.json', function(data) {
+    // $.getJSON('https://dev.taehub.me/muchwow/assets/json/showcase/data.json', function(data) {
         globalData = data;
         var obj = [Object.keys(globalData.showcase)[0],globalData.showcase.Piano.content1,
         globalData.showcase.Piano.content2,globalData.showcase.Piano.content3,
         globalData.showcase.Piano.content4,globalData.showcase.Piano.content5,
-        globalData.showcase.Piano.pathbg1];    
-        setTimeout(()=>{
-            changeName(obj)
-        },100)
+        globalData.showcase.Piano.pathbg1,globalData.showcase.Piano.pathbg2,
+        globalData.showcase.Piano.pathbg3];    
+        changeName(obj)
     });
 }
 
@@ -37,7 +37,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[0],globalData.showcase.Piano.content1,
                         globalData.showcase.Piano.content2,globalData.showcase.Piano.content3,
                         globalData.showcase.Piano.content4,globalData.showcase.Piano.content5,
-                        globalData.showcase.Piano.pathbg1];
+                        globalData.showcase.Piano.pathbg1,globalData.showcase.Piano.pathbg2,
+                        globalData.showcase.Piano.pathbg3];
                     }break;
                 case (btn[1]):{
                         $(btn[0]).attr('class','nav-link');
@@ -47,7 +48,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[1],globalData.showcase.Violin.content1,
                         globalData.showcase.Violin.content2,globalData.showcase.Violin.content3,
                         globalData.showcase.Violin.content4,globalData.showcase.Violin.content5,
-                        globalData.showcase.Violin.pathbg1];
+                        globalData.showcase.Violin.pathbg1,globalData.showcase.Violin.pathbg2,
+                        globalData.showcase.Violin.pathbg3];
                     }break;
                 case (btn[2]):{
                         for (var i = 0; i < 2; i++) {
@@ -59,7 +61,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[2],globalData.showcase.Cello.content1,
                         globalData.showcase.Cello.content2,globalData.showcase.Cello.content3,
                         globalData.showcase.Cello.content4,globalData.showcase.Cello.content5,
-                        globalData.showcase.Cello.pathbg1];
+                        globalData.showcase.Cello.pathbg1,globalData.showcase.Cello.pathbg2,
+                        globalData.showcase.Cello.pathbg3];
                     }break;
                 case (btn[3]):{
                         for (var i = 0; i < 3; i++) {
@@ -71,7 +74,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[3],globalData.showcase.Guitar.content1,
                         globalData.showcase.Guitar.content2,globalData.showcase.Guitar.content3,
                         globalData.showcase.Guitar.content4,globalData.showcase.Guitar.content5,
-                        globalData.showcase.Guitar.pathbg1];
+                        globalData.showcase.Guitar.pathbg1,globalData.showcase.Guitar.pathbg2,
+                        globalData.showcase.Guitar.pathbg3];
                     }break;
                 case (btn[4]):{
                         for (var i = 0; i < 4; i++) {
@@ -81,7 +85,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[4],globalData.showcase.Harmonica.content1,
                         globalData.showcase.Harmonica.content2,globalData.showcase.Harmonica.content3,
                         globalData.showcase.Harmonica.content4,globalData.showcase.Harmonica.content5,
-                        globalData.showcase.Harmonica.pathbg1];
+                        globalData.showcase.Harmonica.pathbg1,globalData.showcase.Harmonica.pathbg2,
+                        globalData.showcase.Harmonica.pathbg3];
                     }break;
                 case (btn[5]):{
                         for (var i = 0; i < 5; i++) {
@@ -90,7 +95,8 @@ var getContent = (id)=>{
                         obj = [Object.keys(globalData.showcase)[5],globalData.showcase.Saxophone.content1,
                         globalData.showcase.Saxophone.content2,globalData.showcase.Saxophone.content3,
                         globalData.showcase.Saxophone.content4,globalData.showcase.Piano.content5,
-                        globalData.showcase.Saxophone.pathbg1];
+                        globalData.showcase.Saxophone.pathbg1,globalData.showcase.Saxophone.pathbg2,
+                        globalData.showcase.Saxophone.pathbg3];
                     }break;
                 case (btn[6]):{
                         for (var i = 0; i < 6; i++) {
@@ -99,15 +105,32 @@ var getContent = (id)=>{
                         console.log("products")
                     }break;
             }
+            if(id==btn[1]||id==btn[2]||id==btn[4]) {
+                $('.navbar-brand').attr('style','color:white;transition:1s;');
+                if(window.innerWidth>990){
+                    $('.navbar-brand').attr('style','color:white;transition:0.7s;');
+                    $('.nav-link').attr('style','color:white;transition:1s;');
+                    $('#prod-btn').attr('onMouseOver','this.style.color="#fff"');
+                    $('#prod-btn').attr('onMouseOut','this.style.color="#fff"');
+                }
+            }else{
+                $('.navbar-brand').attr('style','color:black;transition:0.7s;');
+                $('.nav-link').attr('style','color:black;transition:1s;');
+                $('#prod-btn').attr('onMouseOver','this.style.color="#fff"');
+                $('#prod-btn').attr('onMouseOut','this.style.color="#000"');
+            }
+            
             changeName(obj)
     });
 }
 
 var changeName = (obj) => {
-    $('#name-ins').html('<h1  class="wow fadeInUp">'+obj[0]+'</h1>');
     $('#pic1').attr('style','background-image: url('+obj[6]+');transition: 0.5s');
+    $('#pic2').attr('style','background-image: url('+obj[7]+');transition: 0.5s');
+    $('#footer').attr('style','background-image: url('+obj[8]+');transition: 0.5s');
+    $('#name-ins').html('<h1  class="wow fadeInUp">'+obj[0]+'</h1>');
     $('#content-1').html('<p  class="wow fadeInUp">'+obj[1]+'</span>');
     $('#content-1').append('<p  class="wow fadeInUp">'+obj[2]+'</span>');
-    $('#content-2').html('<p class="wow fadeInUp">' + obj[3] + '</p>'+
-                        '<p class="wow fadeInUp">' + obj[4] + '</p>');    
+    $('#content-2').html('<div class="wow fadeInUp">' + obj[3] + '</div>'+
+                        '<div class="wow fadeInUp">' + obj[4] + '</div>');    
 }
